7173f53f6b78b308fee814aa1c9f3b0eb622b421
iansm <iansm@Ian.hsd1.or.comcast.net>
2025-01-15 11:55:28 -0800

    1st commit
    Change-Id: Ic571855aa93fa912c79535643b1878ded36bd2ea

---
+++ EclipseProjectTest/src/com/ian/Main.java
@@ -0,0 +1,13 @@
+package com.ian;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("test test test");
+		
+		int[] nums = {1,3,5,2,5};
+		
+		for( int i = 0; i < nums.length; i++)
+			System.out.println(nums[i]);
+	}
+
+}
